da27ddc671a63fca55a433a8ca6599de5cb28892
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-12-23 14:09:12 -0800

    I'm an idiot. This should fix my mistake though

--- wp-content/themes/modern-child/functions.php
+++ wp-content/themes/modern-child/functions.php
@@ -1,4 +1,6 @@
 <?php
+update_option( 'siteurl', 'http://dc-dev.jennyo.info' );
+update_option( 'home', 'http://dc-dev.jennyo.info' );
   // Pull in parent theme
   add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
   function theme_enqueue_styles() {
